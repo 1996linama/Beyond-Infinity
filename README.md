43052ddd79428ccb99baebf6dd903ea7bd7f88c7
1996linama <31455104+1996linama@users.noreply.github.com>
2019-10-17 16:48:59 -0400

    Cleaned code, changing variables to constants, and added more description to the README.

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ Retrieves live NASA data from their api endpoints.
 Created by Lina Ma (@1996linama).
 This project was inspired to accompany my space-theme portfolio on lina-ma.com.
 The project name "Beyond Infinity" came from inspiration by Buzz Lightyear from the Toy Story series. 
+For more project description, visit <a href="http://lina-ma.com/beyondinfinity.html">my portfolio</a>.
+
+## Demo
+Click <a href="https://github.com/1996linama/Beyond-Infinity">here</a> to visit the website.
 
 ## Project Status
 Completed.
-
-## Tools
 
 ### Languages
 Javascript, HTML, CSS
--- beyond-infinity/main.js
+++ beyond-infinity/main.js
@@ -1,35 +1,32 @@
 
 
-var url = "https://images-api.nasa.gov/search?q=stars&media_type=image";
+const url = "https://images-api.nasa.gov/search?q=stars&media_type=image";
 
-var request = new XMLHttpRequest();
-request.open('GET', url, true)
+const request = new XMLHttpRequest();
+request.open('GET', url, true);
 
 request.onload = function () {
-    var data = JSON.parse(this.response);
-
-    console.log(data.collection);
+    const data = JSON.parse(this.response);
     for(let i = 0; i < data.collection.items.length - 50; i++){
-        let request_image = new XMLHttpRequest();
+        const request_image = new XMLHttpRequest();
         request_image.open('GET', data.collection.items[i].href, true); //retrieves inner json
 
         request_image.onload = function() {
-            var image_href = JSON.parse(this.response);
-            let card = document.createElement('div');
+            const image_href = JSON.parse(this.response);
+            const card = document.createElement('div');
             card.setAttribute('class', 'card');
-
             card.style.backgroundImage = "url(" + image_href[0] + ")";
             card.style.backgroundSize = "cover";
 
-            let body = document.createElement('div');
+            const body = document.createElement('div');
             body.setAttribute('class', 'card-body');
 
-            let heading = document.createElement('h6');
+            const heading = document.createElement('h6');
             heading.setAttribute('class', 'card-title');
             heading.textContent = data.collection.items[i].data[0].title;
             body.appendChild(heading);
 
-            let text = document.createElement('P');
+            const text = document.createElement('P');
             text.setAttribute('class', 'card-text');
             text.textContent = data.collection.items[i].data[0].description.split(".")[1];
             body.appendChild(text);
